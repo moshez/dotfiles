49c63cbb5fcda7c6ff94c59ef5ddbd3733e309eb
Moshe Zadka <moshez@zadka.club>
2024-01-10 23:21:48 +0000

    checkpoint

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pipx list > /dev/null || python -m pip install --user pipx
-pipx install dotmoshez
+python -m pipx list > /dev/null || python -m pip install --user pipx
+python -m pipx install dotmoshez
 ~/.local/bin/dotmoshez bash-install --no-dry-run --dotfiles ~/src/dotfiles/ --no-dry-run
